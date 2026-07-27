6b5367ebfdfd711653cbb2a87bd57a7b4169ee46
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2026-07-27 18:37:59 -0300

    refactor: inline `createRouter()` chain in all API router exports

--- pages/api/v1/activations/[token_id]/index.js
+++ pages/api/v1/activations/[token_id]/index.js
@@ -3,12 +3,10 @@ import activation from "models/activation";
 import authorization from "models/authorization.js";
 import { createRouter } from "next-connect";
 
-const router = createRouter();
-
-export default router.handler(controller.errorHandlers);
-
-router.use(controller.injectAnonymousOrUser);
-router.patch(controller.canRequest("read:activation_token"), patchHandler);
+export default createRouter()
+  .use(controller.injectAnonymousOrUser)
+  .patch(controller.canRequest("read:activation_token"), patchHandler)
+  .handler(controller.errorHandlers);
 
 async function patchHandler(request, response) {
   const userTryingToPatch = request.context.user;
