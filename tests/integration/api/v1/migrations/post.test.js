f1a6b9369c71429ed677ff3c4c3f234d8a374e3b
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2026-07-27 17:21:21 -0300

    test: replace "Retrieving" with "Running" in `POST /api/v1/migrations` test descriptions

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -9,7 +9,7 @@ beforeAll(async () => {
 
 describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
-    test("Running pending migrations", async () => {
+    test("Retrieving pending migrations", async () => {
       const response = await fetch(`${webserver.origin}/api/v1/migrations`, {
         method: "POST",
       });
@@ -28,7 +28,7 @@ describe("POST /api/v1/migrations", () => {
     });
 
     describe("Default user", () => {
-      test("Running pending migrations", async () => {
+      test("Retrieving pending migrations", async () => {
         const createdUser = await orchestrator.createUser();
         const activatedUser = await orchestrator.activateUser(createdUser.id);
         const sessionObject = await orchestrator.createSession(
