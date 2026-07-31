9996c70dac4245a227b0e1ff6d4b10c652a7ff67
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2026-07-31 16:21:31 -0300

    feat: install and configure GitHub Primer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import "../global.css";
+import "@primer/primitives/dist/css/functional/themes/light.css";
+
+import { ThemeProvider, BaseStyles } from "@primer/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <BaseStyles>
+        <Component {...pageProps} />
+      </BaseStyles>
+    </ThemeProvider>
+  );
 }
