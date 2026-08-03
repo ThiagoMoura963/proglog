40d617a44b956e988443af37755909ebb81edec8
ThiagoMoura963 <thiagomjfelis3@gmail.com>
2026-08-03 18:30:25 -0300

    feat: implement registration page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@primer/primitives/dist/css/functional/themes/light.css";
+import "@primer/primitives/dist/css/base/motion/motion.css";
 
 import { ThemeProvider, BaseStyles } from "@primer/react";
 
--- pages/cadastro/index.js
+++ pages/cadastro/index.js
@@ -1,71 +1,121 @@
-import { useState } from "react";
-import { Button } from "@primer/react";
+import DefaultLayout from "pages/interface/DefaultLayout/index.js";
+import FormField from "pages/interface/FormField/index.js";
+
+import { useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { Button, Heading, Stack, TextInput } from "@primer/react";
+import { EyeIcon, EyeClosedIcon } from "@primer/octicons-react";
 
 export default function RegisterPage() {
-  console.log("Render do <RegisterPage>");
+  const router = useRouter();
 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const passwordInputRef = useRef(null);
+
+  function focusAfterEnd() {
+    setTimeout(() => {
+      const input = passwordInputRef.current;
+      const len = input.value.length;
+      input.focus();
+      input.setSelectionRange(len, len);
+    });
+  }
+
+  function handleTogglePassword(event) {
+    event.preventDefault();
+    setShowPassword((prev) => !prev);
+    focusAfterEnd();
+  }
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const requestBody = { username, email, password };
+    setIsLoading(true);
 
-    console.log(requestBody);
+    try {
+      const requestBody = { username, email, password };
+      const response = await fetch("/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
 
-    const response = await fetch("/api/v1/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestBody),
-    });
+      if (response.status === 201) {
+        await router.push("/cadastro/confirmar");
+        return;
+      }
 
-    if (response.status === 201) {
-      location.href = "/cadastro/confirmar";
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
     }
   }
 
   return (
-    <>
-      <h1>Cadastro</h1>
+    <DefaultLayout title="Cadastro">
+      <Stack align="center">
+        <Heading as="h1">Cadastro</Heading>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          Nome de usuário:
-          <input
-            type="text"
-            value={username}
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-        </div>
+        <form
+          onSubmit={handleSubmit}
+          style={{ width: "100%", maxWidth: "400px" }}
+        >
+          <Stack gap="normal">
+            <FormField
+              label="Nome de usuário"
+              size="large"
+              autoComplete="username"
+              type="text"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
 
-        <div>
-          Email:
-          <input
-            type="text"
-            value={email}
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-          />
-        </div>
+            <FormField
+              label="Email"
+              size="large"
+              autoComplete="email"
+              type="text"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
 
-        <div>
-          Senha:
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-
-        <Button variant="primary">Criar cadastro</Button>
-      </form>
-    </>
+            <FormField
+              ref={passwordInputRef}
+              label="Senha"
+              size="large"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              autoComplete="new-password"
+              onChange={(event) => setPassword(event.target.value)}
+              trailingAction={
+                <TextInput.Action
+                  onClick={handleTogglePassword}
+                  icon={showPassword ? EyeIcon : EyeClosedIcon}
+                  aria-label={showPassword ? "Esconder senha" : "Mostrar senha"}
+                />
+              }
+            />
+            <Button
+              block
+              type="submit"
+              variant="primary"
+              size="large"
+              disabled={isLoading}
+              loading={isLoading}
+            >
+              Criar cadastro
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
+    </DefaultLayout>
   );
 }
